a295fcc8276c8155431572cdd6897e462f00e39d
Exydous <benedictusaryantiow@gmail.com>
2026-04-20 13:00:57 +0700

    fix bug

---
+++ lib/core/constants/api_keys.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiKeys {
+  // Mengambil nilai dari .env, jika tidak ada akan return string kosong
+  static String supabaseUrl = dotenv.get('SUPABASE_URL', fallback: '');
+  static String supabaseAnonKey = dotenv.get('SUPABASE_ANON_KEY', fallback: '');
+}
